57286a0ab4c845426c02e50deb778fabc31ad90d
ozkarta <ozbegi1@gmail.com>
2017-03-20 22:23:14 +0400

    task 1 is done

---
+++ c#/davaleba_1/gtu_task1/gtu_task1/Module1.vb
@@ -0,0 +1,116 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim arr(10) As Integer
+
+        Dim list As List(Of Integer) = New List(Of Integer)
+
+        arr(0) = 12
+        arr(1) = 18
+        arr(2) = -20
+        arr(3) = 41
+        arr(4) = 0
+        arr(5) = 21
+        arr(6) = -11
+        arr(7) = 30
+        arr(8) = 17
+        arr(9) = 12113
+
+        For Each i As Integer In arr
+            list.Add(i)
+        Next
+
+        Console.WriteLine("            !!!NOT Sorted!!!            ")
+        Console.WriteLine("_____________array________________")
+
+        For Each value In arr
+            Console.Write(value & "     ")
+        Next
+
+
+        Console.WriteLine("_____________LIST___________")
+
+        For Each value In list
+            Console.Write(value & "     ")
+        Next
+
+        sortArray(arr)
+        sortList(list)
+
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+
+
+        Console.WriteLine("            !!!   !!!!!!!!!   !!!            ")
+        Console.WriteLine("_____________array________________")
+
+        For Each value In arr
+            Console.Write(value & "     ")
+        Next
+
+
+        Console.WriteLine("_____________LIST___________")
+
+        For Each value In list
+            Console.Write(value & "     ")
+        Next
+
+
+
+
+
+
+
+
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+    Sub sortArray(ByRef arr() As Integer)
+
+        Dim swap As Integer = 0
+
+
+        For i As Integer = 1 To arr.Length - 1
+            If arr(i) < arr(i - 1) Then
+                swap = arr(i)
+                arr(i) = arr(i - 1)
+                arr(i - 1) = swap
+
+                sortArray(arr)
+
+                Return
+
+            End If
+        Next
+
+
+    End Sub
+
+
+    Sub sortList(ByRef list As List(Of Integer))
+
+        Dim swap As Integer = 0
+
+
+        For i As Integer = 1 To list.Count - 1
+            If list(i) < list(i - 1) Then
+                list.Add(list(i - 1))
+                list.RemoveAt(i - 1)
+                sortList(list)
+
+                Return
+
+            End If
+        Next
+
+
+    End Sub
+
+End Module
---
+++ c#/davaleba_1/gtu_task1/gtu_task1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("gtu_task1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("gtu_task1")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("70b2bbd7-b2b9-4102-8d0d-9e002b29651a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
