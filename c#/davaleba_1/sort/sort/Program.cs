6fc4b550b1ef103f8de9d0dcfb02257e4cac9bc8
ozkarta <ozbegi1@gmail.com>
2017-03-23 20:48:08 +0400

    custom sorting algorithm

---
+++ c#/davaleba_1/sort/sort/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr=new int[10];
+            arr[0] = 10;
+            arr[1] = 0;
+            arr[2] = 13;
+            arr[3] = -2;
+            arr[4] = -20;
+            arr[5] = 10;
+            arr[6] = 10;
+            arr[7] = 17;
+            arr[8] = 3;
+            arr[9] = 13;
+
+            sortTRIAL(arr);
+
+
+
+            Console.ReadKey();
+
+        }
+
+        public static void sortTRIAL(int[] arr)
+        {
+            int?[] staging = new int?[arr.Length];
+            int[] counterArr = new int[arr.Length];
+
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                staging[i] = null;
+
+                int counter = 0;
+                foreach (int var in arr)
+                {
+                    if (arr[i] >= var)
+                    {
+                        counter++;
+                    }
+                }
+                counterArr[i] = counter-1;
+                Console.WriteLine(counterArr[i]);
+                
+            }
+
+            for (int i = 0; i < counterArr .Length; i++)
+            {
+                int counter = 0;
+                
+                while(staging[counterArr[i] + counter]==arr[i])
+                {
+                    counter--;
+                }
+                staging[counterArr[i]+counter] = arr[i];
+                
+            }
+
+            Console.WriteLine("_______________________");
+            for (int i = 0; i < staging.Length; i++)
+            {
+                Console.WriteLine(staging[i]);
+            }
+
+
+
+        }
+    }
+}
